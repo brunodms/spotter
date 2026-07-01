55472d43cfdc5a2c2581d4f1483461bd2f07e063
bruno.macedo <brunomacedosana@gmail.com>
2026-07-01 17:42:29 -0300

    adicionada landing page e margem na seção ativos

--- core/views/auth.py
+++ core/views/auth.py
@@ -13,8 +13,17 @@ from .personal.dashboard import PersonalDashboardView
 
 
 class HomeView(TemplateView):
+    template_name = "core/landing.html"
+
     def dispatch(self, request, *args, **kwargs):
-        return redirect("core:dashboard")
+        if request.user.is_authenticated:
+            return redirect("core:dashboard")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["slide_range"] = range(8)
+        return context
 
 
 class RegistroView(CreateView):
